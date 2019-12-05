d500f52baa425c43032e358a600c168b9591439e
ruben <rust.ruben@gmail.com>
2019-12-05 10:57:48 +0100

    lieuxEvenement lien ruben

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,7 @@ import {OrgaEvenementComponent} from './orga-evenement/orga-evenement.component'
 import {IncriptionFormComponent} from "./incription-form/incription-form.component";
 import {NouveauLieuFormComponent} from "./nouveau-lieu-form/nouveau-lieu-form.component";
 import {InscriptionEntrepriseComponent} from './inscription-entreprise/inscription-entreprise.component';
-import {RouterModule, Routes} from "@angular/router";
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
+
 
 
 const routes: Routes = [{path: 'orgaEvenement', component: OrgaEvenementComponent},
