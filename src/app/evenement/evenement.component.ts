8fbe9a07939a2b30c44332acd877793ffc7996d1
ruben <rust.ruben@gmail.com>
2019-12-05 10:42:12 +0100

    début des liens evenementOrga ruben

--- src/app/evenement/evenement.component.ts
+++ src/app/evenement/evenement.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'evenement',
-  templateUrl: './evenement.component.html',
+  templateUrl: './evenement-page.component.html',
   styleUrls: ['./evenement.component.css']
 })
 export class EvenementComponent implements OnInit {
--- src/app/lieux-evenement/lieux-evenement.component.ts
+++ src/app/lieux-evenement/lieux-evenement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AppConfigService} from '../app-config.service';
 
 @Component({
   selector: 'app-lieux-evenement',
@@ -8,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class LieuxEvenementComponent implements OnInit {
 
   constructor() { }
+
 
   ngOnInit() {
   }
