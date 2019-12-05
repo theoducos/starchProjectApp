da33feee3dbbd825eb3d28a6917f7294c77cb02d
Theo <ducostheo1@gmail.com>
2019-12-05 10:50:22 +0100

    eventv1

--- src/app/evenement/evenement.component.ts
+++ src/app/evenement/evenement.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {Evenement} from '../model/evenement';
+import {EvenementHttpService} from './evenement-http.service';
 
 @Component({
   selector: 'evenement',
-  templateUrl: './evenement.component.html',
+  templateUrl: './evenement-page.component.html',
   styleUrls: ['./evenement.component.css']
 })
 export class EvenementComponent implements OnInit {
+evenement: Evenement =null;
 
-  constructor() { }
+  constructor(private evenementService: EvenementHttpService) { }
 
   ngOnInit() {
+  }
+
+  list(): any{
+    return this.evenementService.findAll();
+
+  }
+
+  add() {
+    this.evenement = new Evenement();
+  }
+
+  edit(id:number) {
+    this.evenementService.findById(id).subscribe(resp => this.evenement = resp);
+  }
+  delete(id: number) {
+    this.evenementService.deleteBydId(id);
+  }
+
+  cancel() {
+    this.evenement = null;
   }
 
 }
