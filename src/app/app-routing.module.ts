b678b8f527f7875f420546796848cf4cd77482bf
Oriane <orianegalmar@gmail.com>
2019-12-05 11:01:48 +0100

    Création des Http Services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {NouveauLieuFormComponent} from "./nouveau-lieu-form/nouveau-lieu-form.co
 import {InscriptionEntrepriseComponent} from './inscription-entreprise/inscription-entreprise.component';
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {EvenementComponent} from "./evenement/evenement.component";
 
 
 const routes: Routes = [{path: 'orgaEvenement', component: OrgaEvenementComponent},
@@ -12,7 +13,8 @@ const routes: Routes = [{path: 'orgaEvenement', component: OrgaEvenementComponen
   {path: 'inscriptionEntreprise', component: InscriptionEntrepriseComponent},
   {path: 'nouveauLieu', component: NouveauLieuFormComponent},
   {path: 'orgaEvenement', component: OrgaEvenementComponent},
-  {path: 'inscriptionEntreprise', component: InscriptionEntrepriseComponent}
+  {path: 'inscriptionEntreprise', component: InscriptionEntrepriseComponent},
+  {path: 'evenement', component: EvenementComponent}
 ];
 
 
