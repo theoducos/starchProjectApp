8aeeae68184a5498658a1921fd5ead9387910571
Oriane <orianegalmar@gmail.com>
2019-12-06 15:46:33 +0100

    Pushing Oriane

--- src/app/evenement-starch/evenement-starch.component.ts
+++ src/app/evenement-starch/evenement-starch.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {EvenementStarchHttpService} from './evenement-starch-http.service';
 import {EvenementStarch} from '../model/evenementStarch';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-evenement-starch',
@@ -9,10 +10,13 @@ import {EvenementStarch} from '../model/evenementStarch';
 })
 export class EvenementStarchComponent implements OnInit {
 
-  evenementsStarch: any;
+  img: any;
 
-  constructor(private evenementStarchService: EvenementStarchHttpService) {
+  constructor(private evenementStarchService: EvenementStarchHttpService, private route: ActivatedRoute) {
+    this.route.params.subscribe(resp => {
+      this.img = resp;
 
+    });
   }
 
 
