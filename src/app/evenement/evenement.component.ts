ebf8a59b02ae003b570032cd8312fcc14a5e5bbb
Oriane <orianegalmar@gmail.com>
2019-12-06 18:05:50 +0100

    Push du soir

--- src/app/evenement/evenement.component.ts
+++ src/app/evenement/evenement.component.ts
@@ -24,7 +24,7 @@ export class EvenementComponent implements OnInit {
   constructor(private evenementHttpService: EvenementHttpService, private activatedRoute: ActivatedRoute, private entrepriseHttpService: EntrepriseHttpService, private utilisateurHttpService: UtilisateurHttpService) {
     this.activatedRoute.params.subscribe(params => {
       this.utilisateurHttpService.findById(params.id).subscribe(resp => {
-        this.utilisateur =resp;
+        this.utilisateur = resp;
         this.utilisateurHttpService.findEntrepriseByUtilisateurId(this.utilisateur.id).subscribe(resp => {
           this.entreprise = resp;
           this.entrepriseHttpService.findEvenementsByEntreprises(this.entreprise.id).subscribe(resp => {
