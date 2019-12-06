68489cdc8852b8d2a3a31531280a228d826bdf45
ruben <rust.ruben@gmail.com>
2019-12-06 17:45:27 +0100

    push du soir

--- src/app/evenement/evenement.component.ts
+++ src/app/evenement/evenement.component.ts
@@ -21,18 +21,22 @@ export class EvenementComponent implements OnInit {
   evenement: Evenement = new Evenement();
   utilisateur: Utilisateur = new Utilisateur();
 
-  constructor(private evenementHttpService: EvenementHttpService, private activatedRoute: ActivatedRoute, private entrepriseHttpService: EntrepriseHttpService, private utilisateurHttpService: UtilisateurHttpService) {
-    this.activatedRoute.params.subscribe(params => {
+  constructor(private evenementHttpService: EvenementHttpService, private route: ActivatedRoute, private entrepriseHttpService: EntrepriseHttpService, private utilisateurHttpService: UtilisateurHttpService) {
+    // this.utilisateur.id = localStorage.getItem('id') as unknown as number;
+
+    this.route.params.subscribe(params => {
       this.utilisateurHttpService.findById(params.id).subscribe(resp => {
-        this.utilisateur =resp;
+        this.utilisateur = resp;
         this.utilisateurHttpService.findEntrepriseByUtilisateurId(this.utilisateur.id).subscribe(resp => {
           this.entreprise = resp;
           this.entrepriseHttpService.findEvenementsByEntreprises(this.entreprise.id).subscribe(resp => {
             this.evenements = resp;
           });
-        })
+        });
       });
-    });
+    })
+
+
   }
 
 
